34ea15baf9f124729871c0ddf6996eecf174bfca
unknown <setyoadisasono@gmail.com>
2023-03-13 21:22:45 +0700

    Tab Layout dengan ViewPager2

--- MyTablayout/app/src/main/java/com/setyo/mytablayout/MainActivity.kt
+++ MyTablayout/app/src/main/java/com/setyo/mytablayout/MainActivity.kt
@@ -2,10 +2,31 @@ package com.setyo.mytablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAB_TITLES: IntArray
+            get() = intArrayOf(
+                R.string.tab_text_1,
+                R.string.tab_text_2
+            )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sectionPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        supportActionBar?.elevation = 0f
     }
 }

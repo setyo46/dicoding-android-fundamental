6e31ab4eab809eda3497ceafe4eef413d13d0a7d
unknown <setyoadisasono@gmail.com>
2023-03-14 16:54:16 +0700

    Using Executor and Handler to create new thread

--- MyBackgroundThread/app/src/main/java/com/setyo/mybackgroundthread/MainActivity.kt
+++ MyBackgroundThread/app/src/main/java/com/setyo/mybackgroundthread/MainActivity.kt
@@ -2,8 +2,11 @@ package com.setyo.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,20 +16,29 @@ class MainActivity : AppCompatActivity() {
         val startButton = findViewById<Button>(R.id.starButton)
         val statusTextView = findViewById<TextView>(R.id.statusTextView)
 
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+
         startButton.setOnClickListener {
-            try {
-                // simulate process compressing
-                for (i in 0..10) {
-                    Thread.sleep(500)
-                    val percentage = i * 100
-                    if (percentage == 100) {
-                        statusTextView.setText(R.string.task_completed)
-                    } else {
-                        statusTextView.text = String.format(getString(R.string.compressing), percentage)
+            executor.execute {
+                try {
+                    // simulate process in background thread
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+                        handler.post {
+                            // update ui in main thread
+                            if (percentage == 100) {
+                                statusTextView.setText(R.string.task_completed)
+                            } else {
+                                statusTextView.text = String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
                     }
+                }catch (e: InterruptedException) {
+                    e.printStackTrace()
                 }
-            }catch (e: InterruptedException) {
-                e.printStackTrace()
             }
         }
     }

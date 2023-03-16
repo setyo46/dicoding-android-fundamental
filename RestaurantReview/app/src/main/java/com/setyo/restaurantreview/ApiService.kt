c3dfb0bf9c6f4a8ffa022d9f7e02fc5efcfc1f58
unknown <setyoadisasono@gmail.com>
2023-03-16 15:55:03 +0700

    Mengirim Data Review

--- RestaurantReview/app/src/main/java/com/setyo/restaurantreview/ApiService.kt
+++ RestaurantReview/app/src/main/java/com/setyo/restaurantreview/ApiService.kt
@@ -10,4 +10,13 @@ interface ApiService {
     fun getRestaurant(
         @Path("id") id: String
     ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: Token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
 }

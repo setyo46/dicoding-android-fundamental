5e5a4e488fdecd2f685c9d58ff9bcfb681ba507f
unknown <setyoadisasono@gmail.com>
2023-03-20 22:05:32 +0700

    menambah apiservice

--- RestaurantReview/app/src/main/java/com/setyo/restaurantreview/ApiConfig.kt
+++ RestaurantReview/app/src/main/java/com/setyo/restaurantreview/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.setyo.restaurantreview
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,8 +9,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
-            val loggingInterceptor =
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+
+            val authInterceptor = Interceptor { chain ->
+                val req = chain.request()
+                val requestHeaders = req.newBuilder()
+                    .addHeader("Authorization", "1234")
+                    .build()
+                chain.proceed(requestHeaders)
+            }
+
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build()

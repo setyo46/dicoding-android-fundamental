24ce4afbf1c363b80a5d9e6b45b5b4baa85ce54a
unknown <setyoadisasono@gmail.com>
2023-03-25 21:30:40 +0700

    Menyimpan Data dengan SharedPreferences

--- MySharedPreferences/app/src/main/java/com/setyo/mysharedpreferences/UserModel.kt
+++ MySharedPreferences/app/src/main/java/com/setyo/mysharedpreferences/UserModel.kt
@@ -1,3 +1,13 @@
 package com.setyo.mysharedpreferences
 
-data class UserModel()
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserModel(
+    var name: String? = null,
+    var email: String? = null,
+    var phoneNumber: String? = null,
+    var age: Int = 0,
+    var isLove: Boolean = false
+): Parcelable

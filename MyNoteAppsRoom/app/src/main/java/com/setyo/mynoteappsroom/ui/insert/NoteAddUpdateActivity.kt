554e3230471106aaeed62f5940ceec6a4ef4c615
unknown <setyoadisasono@gmail.com>
2023-03-31 14:36:47 +0700

    menghubungkan NoteAddUpdateViewModel dengan NoteAddUpdateActivity

--- MyNoteAppsRoom/app/src/main/java/com/setyo/mynoteappsroom/ui/insert/NoteAddUpdateActivity.kt
+++ MyNoteAppsRoom/app/src/main/java/com/setyo/mynoteappsroom/ui/insert/NoteAddUpdateActivity.kt
@@ -2,18 +2,40 @@ package com.setyo.mynoteappsroom.ui.insert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.setyo.mynoteappsroom.R
 import com.setyo.mynoteappsroom.database.Note
+import com.setyo.mynoteappsroom.databinding.ActivityNoteAddUpdateBinding
+import com.setyo.mynoteappsroom.helper.ViewModelFactory
 
 class NoteAddUpdateActivity : AppCompatActivity() {
 
     private var isEdit = false
     private var note: Note? = null
 
+    private lateinit var noteAddUpdateViewModel: NoteAddUpdateViewModel
+
+    private var _activityNoteAddUpdateBinding: ActivityNoteAddUpdateBinding? = null
+    private val binding get() = _activityNoteAddUpdateBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_add_update)
+        _activityNoteAddUpdateBinding = ActivityNoteAddUpdateBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        noteAddUpdateViewModel = obtainViewModel(this@NoteAddUpdateActivity)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityNoteAddUpdateBinding = null
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(NoteAddUpdateViewModel::class.java)
+    }
+
 
     companion object {
         const val EXTRA_NOTE = "extra_note"

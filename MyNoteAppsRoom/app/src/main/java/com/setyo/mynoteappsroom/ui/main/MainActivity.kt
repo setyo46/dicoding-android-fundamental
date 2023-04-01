ddd24335c2770c8682e0a7f6d23dff792371e728
unknown <setyoadisasono@gmail.com>
2023-04-01 20:54:55 +0700

    untuk menghapus item dari database

--- MyNoteAppsRoom/app/src/main/java/com/setyo/mynoteappsroom/ui/main/MainActivity.kt
+++ MyNoteAppsRoom/app/src/main/java/com/setyo/mynoteappsroom/ui/main/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.setyo.mynoteappsroom.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.setyo.mynoteappsroom.R
+import com.setyo.mynoteappsroom.databinding.ActivityMainBinding
+import com.setyo.mynoteappsroom.helper.ViewModelFactory
+import com.setyo.mynoteappsroom.ui.insert.NoteAddUpdateActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+
+    private lateinit var adapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        adapter = NoteAdapter()
+
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+        
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this) { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        }
+
+        binding?.fabAdd?.setOnClickListener { view ->
+            if (view.id == R.id.fab_add) {
+                val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory)[MainViewModel::class.java]
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
     }
 }
